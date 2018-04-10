0daaa20971bb3bb1084f1fa471c685aa866e25f0
Tyler Haun <frodo03211@gmail.com>
2018-04-10 01:27:18 -0700

    fixed get me route

--- server/routes/me.js
+++ server/routes/me.js
@@ -4,7 +4,13 @@ module.exports = function(app, route) {
 
     app.route(route)
         .get(function main(request, response, next) {
-            return response.status(401).send();
+            console.log("SESSION", request.session);
+            console.log("USER", request.user);
+            
+            if (request.user) {
+                return response.json({user: request.user});
+            }
+            return response.status(401).json({errors: ["Unauthenticated"]});
             //return response.json({test});
         })
 
