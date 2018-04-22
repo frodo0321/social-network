d7cf83d3e42f56ea493839faf2e8fcbe8678051f
Tyler Haun <frodo03211@gmail.com>
2018-04-21 17:44:51 -0700

    hid toolbar menus on auth view

--- client/components/toolbar/toolbar.js
+++ client/components/toolbar/toolbar.js
@@ -1,8 +1,20 @@
 angular.module("app")
 .component("toolbar", {
     templateUrl: "toolbar",
-    controller: function toolbarController($scope, $api, $state) {
-        console.log("TOOLBAR CONTROLLER");
+    controller: function toolbarController($scope, $api, $state, $transitions) {
+        console.log("TOOLBAR CONTROLLER", $scope);
+
+        $transitions.onSuccess({}, function(transition) {
+            console.log("TRANSITIONrwihfi", transition.to());
+            if (transition.to().name == "auth") {
+                $scope.showMenus = false;
+            }
+            else {
+                if ($scope.showMenus != true) {
+                    $scope.showMenus = true;
+                }
+            }
+        });
 
         $scope.openMenu = function($mdMenu, ev) {
             originatorEv = ev;
